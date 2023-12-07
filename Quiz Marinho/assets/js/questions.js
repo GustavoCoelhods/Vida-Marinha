1ecc9dcfc8d117e7bb02da5e37113b40d304fc6e
Gucs1 <Gustavoschimidt23@gmail.com>
2023-12-06 21:50:52 -0300

    Projeto Vida Marinha

---
+++ Quiz Marinho/assets/js/questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+    numb: 1,
+    question: "Qual o maior mamífero do oceano",
+    answer: "Baleia Azul",
+    options: [
+      "Baleia Azul",
+      "Peixe Boi",
+      "Baleia Jubarte",
+      "Leão Marinho"
+    ]
+  },
+    {
+    numb: 2,
+    question: "Qual a maior tartaruga marinha?",
+    answer: "Tartaruga de couro",
+    options: [
+      "Tartaruga de pente",
+      "Tartaruga oliva",
+      "Tartaruga de couro",
+      "Tartaruga verde"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Qual oceano é o mais poluido do mundo?",
+    answer: "Oceano Pacífico",
+    options: [
+      "Oceano Índico",
+      "Oceano Pacífico",
+      "Oceano Ártico",
+      "Oceano Atlântico"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Qual animal se habita na fauna brasileira?",
+    answer: "Peixe Boi",
+    options: [
+      "Raia",
+      "Orca",
+      "Tubarão Baleia",
+      "Peixe Boi"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Qual o tipo de lixo mais jogado nos oceanos?",
+    answer: "Plástico",
+    options: [
+      "Plástico",
+      "Vidro",
+      "Isopor",
+      "Canudo"
+    ]
+  },
+];
